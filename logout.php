71b906ceeef18fb771b3acde087a70e1735c6e74
PrincMullatahiri <princmullatahiri@hotmail.com>
2017-05-27 16:50:20 +0200

    no message

--- logout.php
+++ logout.php
@@ -3,6 +3,8 @@
 session_start();
 $_SESSION['username']="NoUserActive";
 
+
+echo("<script>location.href = 'login.php';</script>");
 //session_destroy('username');
 
 //header("Location:login.php");
